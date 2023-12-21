01490c6d252d996253c439cfff0303c42302bb22
Nurul Amin <namin2750@gmail.com>
2023-12-21 23:45:09 +0600

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home.jsx";
-import Users from "./pages/users/Users.jsx";
-import Products from "./pages/products/Products.jsx";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );

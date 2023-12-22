bed221911abf679afb2011ffdc3ce13ecd0eff3c
Nurul Amin <namin2750@gmail.com>
2023-12-22 21:45:39 +0600

    widgets design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Widgets from "../../components/widgets/Widget";
 import "./home.scss";
 
 const Home = () => {
@@ -8,7 +9,12 @@ const Home = () => {
       <Sidebar />
       <div className="home-container">
         <Navbar />
-        Home Container
+        <div className="widgets">
+          <Widgets type="user" />
+          <Widgets type="order" />
+          <Widgets type="earning" />
+          <Widgets type="balance" />
+        </div>
       </div>
     </div>
   );

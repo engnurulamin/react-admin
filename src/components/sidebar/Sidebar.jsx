34925276c8731705bd0572e310394628bd966be2
Nurul Amin <namin2750@gmail.com>
2023-12-22 17:45:20 +0600

    use more icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
@@ -22,37 +32,47 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <li>
-            <PeopleOutlinedIcon />
+            <PersonOutlineIcon />
             <span>Users</span>
           </li>
           <li>
+            <StoreIcon />
             <span>Products</span>
           </li>
           <li>
+            <CreditCardIcon />
             <span>Orders</span>
           </li>
           <li>
+            <LocalShippingIcon />
             <span>Delivery</span>
           </li>
           <li>
+            <InsertChartIcon />
             <span>Stats</span>
           </li>
           <li>
+            <NotificationsNoneIcon />
             <span>Notifications</span>
           </li>
           <li>
+            <SettingsSystemDaydreamOutlinedIcon />
             <span>System Health</span>
           </li>
           <li>
+            <PsychologyOutlinedIcon />
             <span>Logs</span>
           </li>
           <li>
+            <SettingsApplicationsIcon />
             <span>Settings</span>
           </li>
           <li>
+            <AccountCircleOutlinedIcon />
             <span>Profile</span>
           </li>
           <li>
+            <ExitToAppIcon />
             <span>Logout</span>
           </li>
         </ul>

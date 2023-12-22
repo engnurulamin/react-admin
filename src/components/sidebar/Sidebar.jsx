46d9be807c2cd65d2e63768da846dff4f5136b8b
Nurul Amin <namin2750@gmail.com>
2023-12-22 16:56:49 +0600

    material UI

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./sidebar.scss";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">
+          <Link className="logo-link" to="/">
+            Admin
+          </Link>
+        </span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">Color Options</div>
+    </div>
+  );
 };
 
 export default Sidebar;

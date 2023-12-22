5975d742425babb2cb7d598170c7032ab200870d
Nurul Amin <namin2750@gmail.com>
2023-12-22 18:22:54 +0600

    sidebar design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -27,57 +27,69 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
-            <DashboardIcon />
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <PersonOutlineIcon />
+            <PersonOutlineIcon className="icon" />
             <span>Users</span>
           </li>
           <li>
-            <StoreIcon />
+            <StoreIcon className="icon" />
             <span>Products</span>
           </li>
           <li>
-            <CreditCardIcon />
+            <CreditCardIcon className="icon" />
             <span>Orders</span>
           </li>
           <li>
-            <LocalShippingIcon />
+            <LocalShippingIcon className="icon" />
             <span>Delivery</span>
           </li>
+          <p className="title">USEFUL</p>
           <li>
-            <InsertChartIcon />
+            <InsertChartIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon className="icon" />
             <span>Notifications</span>
           </li>
+          <p className="title">SERVICE</p>
           <li>
-            <SettingsSystemDaydreamOutlinedIcon />
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
             <span>System Health</span>
           </li>
           <li>
-            <PsychologyOutlinedIcon />
+            <PsychologyOutlinedIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
-            <SettingsApplicationsIcon />
+            <SettingsApplicationsIcon className="icon" />
             <span>Settings</span>
           </li>
+          <p className="title">USER</p>
           <li>
-            <AccountCircleOutlinedIcon />
+            <AccountCircleOutlinedIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
-            <ExitToAppIcon />
+            <ExitToAppIcon className="icon" />
             <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">Color Options</div>
+      <div className="bottom">
+        <p className="title">THEMES</p>
+        <div className="color-container">
+          <div className="color-option"></div>
+          <div className="color-option"></div>
+          <div className="color-option"></div>
+        </div>
+      </div>
     </div>
   );
 };

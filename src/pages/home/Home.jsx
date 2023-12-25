def0b50c2d0924cbae4fe5caa14c2543587b5a24
Nurul Amin <namin2750@gmail.com>
2023-12-25 20:42:12 +0600

    table component init

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Table from "../../components/table/Table";
 import Widgets from "../../components/widgets/Widget";
 import "./home.scss";
 
@@ -20,6 +21,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="list-container">
+          <p className="list-title">Latest Transactions</p>
+          <Table />
         </div>
       </div>
     </div>

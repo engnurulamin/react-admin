2d3e490b0b22af103dd95b960050d8af989d7aee
Nurul Amin <namin2750@gmail.com>
2023-12-25 22:19:03 +0600

    package

---
+++ src/components/datatable/DataTable.jsx
@@ -0,0 +1,7 @@
+import "./dataTable.scss";
+
+const DataTable = () => {
+  return <div>DataTable</div>;
+};
+
+export default DataTable;
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import DataTable from "../../components/datatable/DataTable";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./list.scss";
 
 const List = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="list-container">
+        <Navbar />
+        <DataTable />
+      </div>
+    </div>
+  );
 };
 
 export default List;

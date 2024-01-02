a28adf0d69b0997891ed4ed04fe6373d05567c8b
Nurul Amin <namin2750@gmail.com>
2024-01-02 21:06:08 +0600

    single page user details

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,52 @@
-import React from "react";
 import "./single.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Single = () => {
-  return <div className="single">Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="single-container">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="edit-btn">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="item-img"
+              />
+              <div className="details">
+                <h1 className="item-title">Jane Doe</h1>
+                <div className="detail-item">
+                  <span className="item-key">Email:</span>
+                  <span className="item-value">janedoe@gmail.com</span>
+                </div>
+                <div className="detail-item">
+                  <span className="item-key">Phone:</span>
+                  <span className="item-value">+1 2345 67 89</span>
+                </div>
+                <div className="detail-item">
+                  <span className="item-key">Address:</span>
+                  <span className="item-value">
+                    Elton St. 234 Garden Yd. NewYork
+                  </span>
+                </div>
+                <div className="detail-item">
+                  <span className="item-key">Country:</span>
+                  <span className="item-value">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">Left</div>
+        </div>
+        <div className="bottom">bottom</div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;

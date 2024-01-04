26fc40dcd92e988439c010686bb225f006c4390e
Nurul Amin <namin2750@gmail.com>
2024-01-04 22:30:37 +0600

    login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,15 @@
-import React from "react";
 import "./login.scss";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="login-container">
+        <input type="text" placeholder="username" id="username" />
+        <input type="password" placeholder="password" id="password" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
